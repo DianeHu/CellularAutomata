a4746384ef80af8a6f1ae391745b6e2eaefd45c9
Madhavi Rajiv <madhavi.rajiv@duke.edu>
2017-09-18 18:19:38 -0400

    Update OrangeSchellingCell.java to have new neighbor checking method

--- src/cells/OrangeSchellingCell.java
+++ src/cells/OrangeSchellingCell.java
@@ -16,13 +16,22 @@ public class OrangeSchellingCell extends Cell{
 	private int colNum;
 	private Rectangle block;
 	private ArrayList<Cell> neighbors;
-	private double satisfiedThreshold = 0.3;
+	private int threshold;
+	private int width;
+	private int height;
 	
 	public OrangeSchellingCell(int myRowNum, int myColNum, int width, int height) {
 		super(myRowNum, myColNum, width, height);
 		block.setFill(Color.DARKORANGE);
 	}
 	
+	public void setThreshold(int t) {
+		threshold = t;
+	}
+	
+	public boolean isNeighbor(int otherRowNum, int otherColNum) {
+		return super.isNeighbor8(otherRowNum, otherColNum);
+	}
 	/* (non-Javadoc)
 	 * @see cells.Cell#moveCell(java.util.ArrayList, cellManager.Grid)
 	 * Overrides superclass method to move cell to random empty location if not satisfied with current location.
