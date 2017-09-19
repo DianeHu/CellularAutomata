ed2e25dade8e12bb9b61815a730a7b2906f38b25
Madhavi Rajiv <madhavi.rajiv@duke.edu>
2017-09-19 14:41:08 -0400

    Update Grid.java for hard coded testing

--- src/cellManager/Grid.java
+++ src/cellManager/Grid.java
@@ -3,16 +3,35 @@ package cellManager;
 import java.io.File;
 import java.util.ArrayList;
 
+import cells.BlueSchellingCell;
 import cells.Cell;
 import cells.EmptyCell;
+import cells.OrangeSchellingCell;
+import cells.SharkCell;
 import javafx.scene.Group;
 
 /**
- * @author Diane Hu
  * @author Madhavi
+ *
+ */
+/**
+ * @author Madhavi
+ *
+ */
+/**
+ * @author Madhavi
+ *
+ */
+/**
+ * @author Madhavi
+ *
+ */
+/**
+ * @author Madhavi
+ *
  */
 public class Grid {
-	
+public static final int SIZE = 400;
 private Group root;
 private Cell[][] currentGrid;
 private Cell[][] newGrid;
@@ -23,25 +42,54 @@ private int numCols;
 private int cellWidth;
 private int cellHeight;
 
-	public Grid(Group r, File f) {
+	public Grid(Group r) {
 		root = r;
-		xml = f;
+		//xml = f;
 		
 	}
 	
-	/**
-	 * Initializes grid with values read from XML file.
-	 */
 	public void initialize() {
-		XMLReader reader = new XMLReader(xml);
+		/*XMLReader reader = new XMLReader(xml);
 		numRows = reader.getNumRows();
-		numCols = reader.getNumCols();
+		numCols = reader.getNumCols();*/
+		numRows = 5;
+		numCols = 5;
+		cellWidth = SIZE/numCols;
+		cellHeight = SIZE/numRows;
 		createEmptyGrid();
 		currentGrid = emptyGrid;
+		setInitialStates();
 		newGrid = emptyGrid;
 		// read from xml to create initial state
 	}
 	
+	private void setInitialStates() {
+		char[][] states ={{'b','b','o','o','o'},
+				    		  {'o','b',' ',' ','b'},
+				    		  {' ',' ','o','b','b'},
+				    		  {'o','o','b','o',' '},
+				    		  {'b',' ','o','b','o'}
+		};
+		for(int i = 0; i<numRows; i++) {
+			for(int j = 0; i<numCols; j++) {
+				if(states[i][j]=='o') {
+					Cell c = new OrangeSchellingCell(i,j,cellWidth,cellHeight);
+					currentGrid[i][j]= c;
+					c.drawCell(root);
+				}
+				if(states[i][j]=='b') {
+					Cell c = new BlueSchellingCell(i,j,cellWidth,cellHeight);
+					currentGrid[i][j]= c;
+					c.drawCell(root);
+				}
+				if(states[i][j]==' ') {
+					Cell c = new EmptyCell(i,j,cellWidth,cellHeight);
+					currentGrid[i][j]= c;
+					c.drawCell(root);
+				}
+			}			
+		}
+	}
 	/**
 	 * Creates a grid with only empty cells which can be used to initialize
 	 *  newgrid and current grid
@@ -53,7 +101,8 @@ private int cellHeight;
 			}
 		}
 	}
-/**
+	
+	/**
 	 * This methods sets the list of neighbors for each cell by checking
 	 *  which of its adjacent cells are considered neighbors by the algorithm
 	 */
@@ -73,39 +122,49 @@ private int cellHeight;
 		ArrayList<Cell> neighbors = new ArrayList<Cell>();
 		int row = cell.getRow(); 
 		int col = cell.getCol();
-		//top
-		if(row!=0 & cell.isNeighbor8(row+1,col)) {
+		for(int i = -1; i<2; i++) {
+			for(int j = -1; j<2; j++) {
+				if(row+i<numRows & row+i>-1 & col+j<numCols & col+j>-1) {
+					if(cell.isNeighbor(row+i,col+j)) {
+						neighbors.add(currentGrid[row+i][col+j]);
+					}
+				}
+			}
+		}		
+		cell.setNeighbors(neighbors);
+		/*//top
+		if(row!=0 & cell.isNeighbor(row+1,col)) {
 			neighbors.add(currentGrid[row-1][col]);
 		}
 		//bottom
-		if(row!=(numRows-1) & cell.isNeighbor8(row-1,col)) {
+		if(row!=(numRows-1) & cell.isNeighbor(row-1,col)) {
 			neighbors.add(currentGrid[row+1][col]);
 		}
 		//left
-		if(col!=0 & cell.isNeighbor8(row,col-1)) {
+		if(col!=0 & cell.isNeighbor(row,col-1)) {
 			neighbors.add(currentGrid[row][col-1]);
 		}
 		//right
-		if(col!=(numCols-1) & cell.isNeighbor8(row,col+1)) {
+		if(col!=(numCols-1) & cell.isNeighbor(row,col+1)) {
 			neighbors.add(currentGrid[row][col+1]);
 		}
 		//upper right
-		if(col!=(numCols-1) & row!=0 & cell.isNeighbor8(row+1,col+1)) {
+		if(col!=(numCols-1) & row!=0 & cell.isNeighbor(row+1,col+1)) {
 			neighbors.add(currentGrid[row-1][col+1]);
 		}
 		//lower right
-		if(row!=(numCols-1) & col!=(numCols-1) & cell.isNeighbor8(row-1,col+1)) {
+		if(row!=(numCols-1) & col!=(numCols-1) & cell.isNeighbor(row-1,col+1)) {
 			neighbors.add(currentGrid[row+1][col+1]);
 		}
 		//upper left
-		if(row!=0 & col!=0 & cell.isNeighbor8(row+1,col-1)) {
+		if(row!=0 & col!=0 & cell.isNeighbor(row+1,col-1)) {
 			neighbors.add(currentGrid[row-1][col-1]);
 		}
 		//lower left
-		if(row!=(numCols-1) & col!=0 & cell.isNeighbor8(row-1,col-1)) {
+		if(row!=(numCols-1) & col!=0 & cell.isNeighbor(row-1,col-1)) {
 			neighbors.add(currentGrid[row+1][col-1]);
 		}
-		cell.setNeighbors(neighbors);
+		cell.setNeighbors(neighbors);*/
 	}
 	
 	public void createsNewGrid() {
@@ -119,9 +178,7 @@ private int cellHeight;
 		}
 	}
 	
-	/**
-	 * @return Returns empty cells of current grid.
-	 */
+	
 	private ArrayList<Cell> getEmptyCells(){
 		ArrayList<Cell> emptyCells = new ArrayList<Cell>();
 		for(int i = 0; i<numRows; i++) {
@@ -137,20 +194,16 @@ private int cellHeight;
 	
 	public void update() {
 		currentGrid = newGrid;
-		newGrid = new Cell[numRows][numCols];
+		newGrid = emptyGrid;
 		for(int i = 0; i<numRows; i++) {
 			for(int j = 0; j<numCols; j++) {
 				Cell c = currentGrid[i][j];
 				c.drawCell(root);
 			}
 		}
+		
 	}
 
-	/**
-	 * @param rownum
-	 * @param colnum
-	 * @return Tests if the new grid has a non-empty cell at a certain location, returns true/false.
-	 */
 	public boolean newGridContainsCellAt(int rownum, int colnum) {
 		if(newGrid[rownum][colnum] instanceof EmptyCell) {
 			return false;
@@ -170,15 +223,11 @@ private int cellHeight;
 		return true;
 	}
 	
-	
-	/**
-	 * @param c
-	 * Adds cell to new grid at cell's designated location.
-	 */
 	public void addToNewGrid(Cell c) {
 		newGrid[c.getRow()][c.getCol()] = c;
 	}
 	
+	//new
 	/**
 	 * @param c is a cell which is removed from the newGrid
 	 * This method removes a cell from the newGrid assuming that the cell has been added
