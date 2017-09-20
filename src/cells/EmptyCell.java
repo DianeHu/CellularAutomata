de6706d667006f554918c00bf956fcd0287f6201
Madhavi Rajiv <madhavi.rajiv@duke.edu>
2017-09-20 17:47:09 -0400

    Update EmptyCell.java to work with Rectangle matrix

--- src/cells/EmptyCell.java
+++ src/cells/EmptyCell.java
@@ -2,50 +2,23 @@ package cells;
 
 import java.util.ArrayList;
 
-import cellManager.Grid;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class EmptyCell extends Cell {
-	private int rowNum;
-	private int colNum;
-	private Rectangle block;
-	private ArrayList<Cell> neighbors;
+
 	
-	public EmptyCell(int myRowNum, int myColNum, int width, int height) {
-		super(myRowNum, myColNum, width, height);
-		block.setFill(Color.FLORALWHITE);
+	public EmptyCell(int myRowNum, int myColNum) {
+		super(myRowNum,myColNum);		
 	}
 	
-	public int getRow() {
-		return super.getRow();
-	}
-	
-	public int getCol() {
-		return super.getCol();
-	}
-	
-	public boolean isSurroundingNeighbor(int otherRowNum, int otherColNum) {
-		return super.isSurroundingNeighbor(otherRowNum, otherColNum);
-	}
-	
-	public void drawCell(Group root) {
-		super.drawCell(root);
-	}
-	
-	public void setNeighbors(ArrayList<Cell> n) {
-		super.setNeighbors(n);
+	public boolean isNeighbor(int otherRowNum, int otherColNum) {
+		return super.isNeighbor8(otherRowNum, otherColNum);
 	}
 	
 	public void moveCell() {
 		//do nothing
-	}
-
-	@Override
-	public void moveCell(ArrayList<Cell> emptySpots, Grid grid) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 
