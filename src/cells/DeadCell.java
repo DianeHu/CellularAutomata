69f001c33c44a301fdb42e5105da5ae145fa67e1
Diane Hu <diane.hu@duke.edu>
2017-09-21 00:03:39 -0400

    Changed moveCell method to add non-moving cells appropriately to new grid

--- src/cells/DeadCell.java
+++ src/cells/DeadCell.java
@@ -10,35 +10,43 @@ import javafx.scene.shape.Rectangle;
 /**
  * @author Diane Hu
  */
-public class DeadCell extends Cell{
-	
-	private boolean resurrected = false;
-	
+public class DeadCell extends Cell {
+
 	public DeadCell(int myRowNum, int myColNum) {
 		super(myRowNum, myColNum);
 		setColor(Color.WHITE);
 	}
-	
+
 	/**
 	 * @param root
-	 * Checks the number of live neighbors. If the number is exactly three, the dead cell is replaced with a live one, as if by reproduction.
-	 * Subsequently resets the number of live neighbors to zero.
+	 *            Checks the number of live neighbors. If the number is exactly
+	 *            three, the dead cell is replaced with a live one, as if by
+	 *            reproduction. Subsequently resets the number of live neighbors to
+	 *            zero.
 	 */
 	public void resurrectCell(Grid newGrid) {
-		checkNumLiveNeighbors();
-		if(getNumLiveNeighbors() == 3) {
-			Cell newCell = new LiveCell(getRow(), getCol());
-			changeCellType(newGrid, newCell);
-			setNumLiveNeighbors(0);
-			resurrected = true;
+		Cell newCell = new LiveCell(getRow(), getCol());
+		newGrid.addToNewGrid(newCell);
+	}
+
+	private boolean shouldResurrect() {
+		if (checkNumLiveNeighbors() == 3) {
+			return true;
 		}
+		return false;
 	}
 
 	@Override
 	public void moveCell(ArrayList<Cell> emptySpots, Grid grid) {
-		resurrectCell(grid);
-		if(resurrected == false) {
+		if(shouldResurrect()) {
+			resurrectCell(grid);
+		} else {
 			grid.addToNewGrid(this);
 		}
+	}
+
+	@Override
+	public boolean isNeighbor(int otherRowNum, int otherColNum) {
+		return super.isNeighbor8(otherRowNum, otherColNum);
 	}
 }
