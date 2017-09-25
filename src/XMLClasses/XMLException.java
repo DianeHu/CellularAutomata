c60c7ca741876ffcc725d215693fe2df0e3031b4
Diane Hu <dh224@duke.edu>
2017-09-24 20:57:23 -0400

    Merged all changes to most recent version

--- src/XMLClasses/XMLException.java
+++ src/XMLClasses/XMLException.java
@@ -1,26 +1,39 @@
 package XMLClasses;
 
+/**
+ * This class enables us to throw exceptions when XML cause errors
+ * 
+ * @author Tyler Yam
+ * Source Code from Robert C. Duvall
+ */
+
 public class XMLException extends RuntimeException {
     // for serialization
     private static final long serialVersionUID = 1L;
 
 
-    /**
-     * Create an exception based on an issue in our code.
+    /** 
+     * @author Tyler Yam
+     * Source Code from Robert C. Duvall
+     * For when our code has an issue
      */
     public XMLException (String message, Object ... values) {
         super(String.format(message, values));
     }
 
     /**
-     * Create an exception based on a caught exception with a different message.
+     *  @author Tyler Yam
+     * Source Code from Robert C. Duvall
+     * For caught exceptions and gives additional messages
      */
     public XMLException (Throwable cause, String message, Object ... values) {
         super(String.format(message, values), cause);
     }
 
     /**
-     * Create an exception based on a caught exception, with no additional message.
+     *  @author Tyler Yam
+     * Source Code from Robert C. Duvall
+     * For caught exceptions but gives no additional messages
      */
     public XMLException (Throwable cause) {
         super(cause);
